77c23fdcd1af6ac76fac766dceae3f8fed773456
Vinay Kumar V <vinay.kumar.v@outlook.com>
2022-09-18 13:57:44 +0530

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
 import { Subscription } from 'rxjs';
 import { AccountService } from '../_services/account.service';
 
@@ -15,7 +14,7 @@ export class NavComponent implements OnInit, OnDestroy {
   loggedInUser: string;
 
   constructor(public accountService: AccountService,
-    private router: Router, private toastr: ToastrService) {}
+    private router: Router) {}
 
   ngOnInit(): void {
 
@@ -28,10 +27,7 @@ export class NavComponent implements OnInit, OnDestroy {
       next: (response) => {
        this.router.navigateByUrl('/members');
       },
-      error: (e) => {
-        console.error(e);
-        this.toastr.error(e.error);
-      },
+      error: e => console.log(e)
     });
 
 
