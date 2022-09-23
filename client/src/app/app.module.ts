9e343d4e5e747bd6faef4792e88ae357c614ce52
Vinay Kumar V <vinay.kumar.v@outlook.com>
2022-09-24 00:56:11 +0530

    End of section 11

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { LoadingInterceptor } from './_interceptors/loading.interceptor';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 
 @NgModule({
   declarations: [
@@ -50,7 +51,8 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
     AddEditEmployeeComponent,
     ShowEmployeeComponent,
     MemberCardComponent,
-    MemberEditComponent
+    MemberEditComponent,
+    PhotoEditorComponent
   ],
   imports: [
     BrowserModule,
@@ -60,7 +62,7 @@ import { LoadingInterceptor } from './_interceptors/loading.interceptor';
     FormsModule,
     ReactiveFormsModule,
     SharedModule,
-    NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' })
+    NgxSpinnerModule.forRoot({ type: 'ball-circus' })
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
